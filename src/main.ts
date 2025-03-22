e89f13cfe1716c95a4f7462c49a9d3ab318deee0
wallace <dev_kollen@outlook.com>
2025-03-21 22:42:23 -0300

    feat: :sparkles: add middleware compression

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(new LoggerMiddleware().use);
+  app.use(compression());
 
   const config = new DocumentBuilder()
     .setTitle('Order & Product API')
